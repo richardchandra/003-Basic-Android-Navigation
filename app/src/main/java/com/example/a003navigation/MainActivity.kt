dcad3eb33b3a4efb15eadbfcbe09b444dda14798
unknown <richardchandra99@gmail.com>
2022-03-15 00:25:29 +0700

    Initial commit for Android Basic Navigation Learning Hands-on Code

---
+++ app/src/main/java/com/example/a003navigation/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a003navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp()||super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/a003navigation/mainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.a003navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentABtn.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragmentA)
+        }
+
+        fragmentBBtn.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragmentB)
+        }
+    }
+}
