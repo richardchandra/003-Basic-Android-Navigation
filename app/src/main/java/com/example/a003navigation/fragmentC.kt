f435f143f31a59b929c475aed826f887c61e24f7
unknown <richardchandra99@gmail.com>
2022-03-16 13:40:09 +0700

    Second push for sending data example

--- app/src/main/java/com/example/a003navigation/fragmentC.kt
+++ app/src/main/java/com/example/a003navigation/fragmentC.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_c.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class FragmentA : Fragment() {
+class fragmentC : Fragment() {
+
+    val args: fragmentCArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false)
+        val view =  inflater.inflate(R.layout.fragment_c, container, false)
+
+        val textMessage = args.message
+        view.messageTextView.text = textMessage
+
+        return view
     }
 
 }
